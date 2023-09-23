863309cfd5e5cf1fb88d81fc14665dfa8fd54b51
Dmitriy Chirkov <StrelokDC@outlook.com>
2023-09-23 11:48:55 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 56000;
+        System.out.println(price);
+        int miles = price / 20;
+        System.out.println(miles);
+
+    }
+}
